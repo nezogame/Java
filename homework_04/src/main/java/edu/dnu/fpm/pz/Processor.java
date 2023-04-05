7b2f3adfd296b9351dd7bdbe17c04983ecc88b60
Denys Hudymov <denys.hudymov@gmail.com>
2023-04-05 12:20:58 +0300

    Fourth lab was created

---
+++ homework_04/src/main/java/edu/dnu/fpm/pz/Processor.java
@@ -0,0 +1,19 @@
+package edu.dnu.fpm.pz;
+
+public class Processor {
+    private Producer producer;
+    private Consumer consumer;
+    public void process() {
+        String value = producer.produce();
+        if (value == null) {
+            throw new IllegalStateException();
+        }
+        consumer.consume(value);
+    }
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
+    public void setConsumer(Consumer consumer) {
+        this.consumer = consumer;
+    }
+}
